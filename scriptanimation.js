8572c4f0bc8b516f6f2599b5473965313c60f2f1
Ivan <i.kravchuk@dunice.net>
2021-12-02 12:59:09 +0300

    4day

--- scriptanimation.js
+++ scriptanimation.js
@@ -1,22 +1,54 @@
-var block = document.getElementById('square');
-var left = 0;
-var down1 = 0;
-var up1 = 0;
-document.onkeydown = function (event){
-    console.log(event);
-    if (event.key == 'ArrowRight'){
-        block.style.right = left + 'px';
-       left++;
-   
+isGameActive = true
+document.addEventListener('readystatechange', () => {
+  if (document.readyState === 'complete') game();
+})
+
+function game() {
+  var div = document.getElementById('border');
+  for (var i = 0; i < 30; i++) { // строки
+    for (var j = 0; j < 40; j++) { // столбца
+      var newDiv = document.createElement('div');
+      newDiv.id = `${i}  ${j}`;
+      newDiv.classList.add('areabox');
+      // newDiv.innerText = `${i}:${j}`;  
+      newDiv.setAttribute('title', `Column - ${j} \nRow - ${i}`);
+      div.appendChild(newDiv);
+
     }
-    if (event.key == 'ArrowDown'){
-        block.style.top = down1 + 'px';
-       down1++;
-   
+  }
+  function square() {
+    for (var i = 0; i < 30; i++) { // строки
+      if (i === 26 || i === 27 || i===25 || i==28 || i==24) {
+        for (var j = 16; j < 20; j++) { 
+          // столбцы
+          document.getElementById(`${i}  ${j}`).classList.remove('areabox');
+          document.getElementById(`${i}  ${j}`).classList.add('card');
+        }
+      }
     }
-    if (event.key == 'ArrowUp'){
-        block.style.all = up1 + 'px';
-       up1++;
-    
-}
+    if (isGameActive) { setTimeout(square, 1000); }
+  }
+  setTimeout(square, 1000);
+  function moveRect(e) {
+
+    var block = document.getElementById("card")
+    var cs = window.getComputedStyle("card");
+
+    var left = parseInt(cs.marginLeft);
+    var top = parseInt(cs.marginTop);
+
+    switch (e.key) {
+      case "ArrowLeft": // нажатие клавиши влево
+        if (left > 0)
+          block.style.marginLeft = left - 10 + "px";
+        break;
+      case "ArrowTop":
+        if (top > 0)
+          blueRect.style.marginTop = top - 10 + "px";
+        break;
+      case "ArrowLeft":
+        if (left > 0)
+          block.style.marginLeft = left - 10 + "px";
+    }
+  }
 }
